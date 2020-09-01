7bc72bfa5ea88797a599749ddeb40edd12483f4d
gachez <briangach98@gmail.com>
2020-09-01 21:52:46 +0300

    added the tests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 const debug =require('debug')('server:debug');
 import config from 'config';
 import express from 'express';
+import bodyParser from 'body-parser';
+import movieRouter from '../routes/movie';
+import mongoose from 'mongoose';
+
+mongoose.connect(config.get('database'), {
+    useNewUrlParser: true
+});
+
+//callback when connection to mongodb is open
+mongoose.connection.once("open", () => {
+  console.log("MongoDB database connection established succesfully");
+});
 
 const app=express();
+
+///support json encoded bodies in the req
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//sets the limit of json bodies in the req body
+
 
 const listen =app.listen(config.get('port'),()=>{
     debug(`server is running on port ${config.get('port')} and in ${config.get('name')} mode`);
